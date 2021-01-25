ef71e05a8f4dfbd5d663177d1fcaf4173fa5ba57
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-25 15:27:01 -0300

    parte 4

--- aluraquiz/pages/_app.js
+++ aluraquiz/pages/_app.js
@@ -1,18 +1,17 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json'
+
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    background-color: #f5f5dc;
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const theme = db.theme
 
 export default function App({ Component, pageProps }) {
   return (
--- aluraquiz/pages/index.js
+++ aluraquiz/pages/index.js
@@ -1,15 +1,17 @@
 import styled from 'styled-components'
 
-/* const Title = styled.h1`
+
+
+const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-` */
+`
 
-function Title(props) {
+/* function Title(props) {  // mesmo do de cima
    return (
       <h1>{props.children}</h1>
    )
-}
+} */
 
 
 export default function Home() {
