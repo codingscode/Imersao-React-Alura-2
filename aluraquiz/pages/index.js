2abac72a680738d411e00e3d76b12ef15ba2fb71
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-25 12:45:23 -0300

    parte 2

--- aluraquiz/pages/index.js
+++ aluraquiz/pages/index.js
@@ -1,10 +1,15 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
+/* const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+` */
+
+function Title() {
+   return <h1>Qualquer título</h1>
+}
+
 
 export default function Home() {
-  return <Title>My page</Title>
+   return <Title>My page</Title>
 }
