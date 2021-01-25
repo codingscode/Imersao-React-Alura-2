d7d3545d2dbea8a049f9d0d3776dac8366a41c50
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-25 16:07:01 -0300

    parte 7

--- aluraquiz/pages/index.js
+++ aluraquiz/pages/index.js
@@ -2,10 +2,6 @@ import styled from 'styled-components'
 import db from '../db.json'
 
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.secondary};
-`
 
 const BackgroundImage = styled.div`
    background-image: url(${db.bg});
@@ -14,11 +10,24 @@ const BackgroundImage = styled.div`
    background-position: center;
 `
 
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`
 
 export default function Home() {
       return (
          <BackgroundImage>
-            Olá pessoas como estão ? ....
+            <QuizContainer>
+               Olá pessoas como estão ? ....
+
+            </QuizContainer>
          </BackgroundImage>
 
       )
