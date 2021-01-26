9ca36bb8074113193f575c610583f14e24e18d87
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-26 20:18:37 -0300

    parte 15

--- aluraquiz/pages/index.js
+++ aluraquiz/pages/index.js
@@ -1,6 +1,7 @@
+import React from 'react'
 import styled from 'styled-components'
 import db from '../db.json'
-import {Widget} from '../src/components/Widget'
+import { Widget } from '../src/components/Widget'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
 import QuizBackground from '../src/components/QuizBackground'
@@ -8,12 +9,7 @@ import QuizLogo from '../src/components/QuizLogo'
 import Head from 'next/head'
 
 
-/* const BackgroundImage = styled.div`
-   background-image: url(${db.bg});
-   flex: 1;
-   background-size: cover;
-   background-position: center;
-` */
+
 
 export const QuizContainer = styled.div`
   width: 100%;
---
+++ aluraquiz/pages/quiz.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function QuizPage() {
+    
+     return (
+         <div style={{color: 'black'}}>
+             Página de Quiz
+         </div>
+     )
+}
