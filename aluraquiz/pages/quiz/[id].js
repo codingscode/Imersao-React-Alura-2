1882bda3d53be138aff3c55b90b2240df930b2e9
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-29 11:33:15 -0300

    parte 42

---
+++ aluraquiz/pages/quiz/[id].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function QuizDaGalera() {
+
+    return (
+       <div style={{color: 'black'}} >
+          Desafio da próxima aula
+       </div>
+    )
+}
