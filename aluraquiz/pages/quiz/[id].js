5596dade48d28c1e97b237600d1bf0410aacf25d
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-29 16:50:54 -0300

    parte 48

--- aluraquiz/pages/quiz/[id].js
+++ aluraquiz/pages/quiz/[id].js
@@ -13,6 +13,21 @@ export default function QuizDaGalera() {
 
 export async function getServerSideProps(context) {
       console.log('Infos que o Next dá para nós: ', context.query)  // { name: 'Aladin', id: 'qualquercoisa' }
+
+      const dbExterno = await fetch('https://aluraquiz-css.omariosouto.vercel.app/api/db')
+         .then((res) => {
+            if(res.ok) {
+               return res.json()
+            }
+            throw new Error('Falha de acesso')
+         })
+         .then((respostaConvertidaEmObjeto) => respostaConvertidaEmObjeto)
+         .catch((erro) => {
+            console.log('houve erro')
+         })
+
+      console.log('dbExterno: \n', dbExterno)
+
       return {
          props: {}
       }
